0f9a4bd624ac69d559a19a76266a79d95f39bb70
Alfredo Martínez <alfre_canos13@icloud.com>
2021-07-12 17:19:35 +0200

    I have removed the outline of the form inputs

--- components/form/Formulary.js
+++ components/form/Formulary.js
@@ -32,10 +32,10 @@ function Formulary () {
         <div className='h-[700px] mx-auto flex flex-col justify-between ls:flex-row items-end h-[640px] w-[89%] mb-[150px] mt-[75px]'>
           <h2 className='w-[70%] h-[20%] ml-0 mr-auto text-sectionTitle pr-8 leading-10 sm:w-[90%] text-left md:w-full text-center ls:w-[25%] text-left h-full desktop:w-[20%] self-start'>Connect with me</h2>
           <Form className='w-full flex flex-col justify-between h-[80%] md:max-w-[550px] mx-auto ls:w-[75%] h-[600px]'>
-            <TextField label='Name' name='name' type='text' />
-            <TextField label='Email' name='email' type='email' />
-            <TextField label='Phone (optional)' name='phone' type='text' />
-            <TextArea label='Message' name='message' />
+            <TextField className='focus:outline-none' label='Name' name='name' type='text' />
+            <TextField className='focus:outline-none' label='Email' name='email' type='email' />
+            <TextField className='focus:outline-none' label='Phone (optional)' name='phone' type='text' />
+            <TextArea className='focus:outline-none' label='Message' name='message' />
             <div className='flex items-center'>
               <Field type='checkbox' name='terms' value='terms' className='w-[5%]' />
               <label htmlFor='terms' className='w-[95%]'>Accept terms and conditions</label>
