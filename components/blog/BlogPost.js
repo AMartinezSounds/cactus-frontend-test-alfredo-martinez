66d48effd450cdc9a5ca6ce34448f3c0f23b7168
Alfredo Martínez <alfre_canos13@icloud.com>
2021-07-13 10:18:50 +0200

    Added hover and active states to the buttons

--- components/blog/BlogPost.js
+++ components/blog/BlogPost.js
@@ -7,7 +7,7 @@ function BlogPost ({ post, image }) {
       <div className='flex flex-col py-8 items-center justify-between h-[500px]'>
         <h2 className='mx-auto w-[90%] text-center text-xl h-[25%]'>{post.title}</h2>
         <Image className='object-contain' src={image} width={150} height={150} alt='blog image' />
-        <div className='readBtn'>
+        <div className='readBtn transition duration-300 ease-in-out hover:bg-secondary active:bg-terciary'>
           <Link href={'/blog/' + post.id} key={post.id}>Read</Link>
         </div>
       </div>
